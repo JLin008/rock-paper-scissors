6e16fa96dd79c6187a10ddb496dc9cf2933bc0d7
JLin008 <jjling0712@gmail.com>
2025-04-11 20:24:39 -0400

    fixed playRound function again and added playGame function

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let choices = [`rock`, `paper`, `scissors`]
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
@@ -11,24 +13,55 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    let winnerOutput = "";
+   
 
     if (humanChoice === computerChoice) {
-        return "It's a tie!";
+        console.log(`It's a tie! You both choose ${humanChoice}`);
+        return;
+
     }
     switch (humanChoice) {
         case "rock":
-            winnerOutput = (computerChoice === "paper") ? "You lose! Paper beats Rock" : "You win! Rock beats Scissors"
+            if (computerChoice === "paper") {
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
+            } 
+            else {
+                console.log("You win! Rock beats Scissors");
+                humanScore++;
+            }
             break;
         case "paper":
-            winnerOutput = (computerChoice === "rock") ? "You lose! Scissor beats Paper" : "You win! Paper beats Rock"
+            if (computerChoice === "scissors") {
+                console.log("You lose! Scissors beats Paper");
+                computerScore++;
+            } 
+            else {
+               console.log("You win! Paper beats Rock");
+                humanScore++;
+            }
             break;
         case "scissors":
-            winnerOutput = (computerChoice === "rock") ? "You lose! Rock beats Scissors" : "You win! Scissors beats Rock"
+            if (computerChoice === "rock") {
+                console.log("You lose! Rock beats Scissors");
+                computerScore++;
+            } 
+            else {
+                console.log("You win! Scissors beats Paper");
+                humanScore++;
+            }
             break;
     }
-    return winnerOutput;
+
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    console.log(`Computer Score: ${computerScore} \n Human Score: ${humanScore}`);
+}
+
+playGame();
